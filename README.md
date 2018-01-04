3db328ee10210bc3b3b84835dcdf0bfa9865bb33
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-01-04 17:40:30 +0300

    Добавлены основные функции и README

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Скрипт принимает на вход путь до текстового файла и выводить в консоль десять самых
+популярных слов в этом файле в порядке убывания частоты.
+
+
+# Как запустить
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+и файла с текстом в формате txt с кодировкой cp1251. 
+
+Запуск на Windows:
+
+```cmd
+
+ C:\5_lang_frequency>python lang_frequency.py <path_to_file>
+
+ Пример:
+ C:\5_lang_frequency>python lang_frequency.py input_file.txt
+ the 7
+ to 6
+ be 4
+ with 3
+ of 3
+ keys 3
+ dictionary 3
+ can 3
+ are 3
+ and 3
+
+```
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,33 @@
-
+import sys
 
 def load_data(filepath):
-    pass
+    words_count = {}
+    with open(filepath) as input_file:
+        for line in input_file:
+            for word in line.split():
+                words_count[word] = words_count.get(word, 0) + 1
+    return words_count
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(file_content):
+    words_frequency = [(frequency, word) for word, frequency in file_content.items()]
+    words_frequency.sort()
+    words_frequency.reverse()
+    words_frequency = [(word, frequency) for frequency, word in words_frequency]
+    return words_frequency
+
+
+def print_most_frequent_words(words_frequency):
+    for word, frequency in words_frequency[:10]:
+            print(word, frequency)
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        file_content = load_data(sys.argv[1])
+        words_frequency = get_most_frequent_words(file_content)
+        print_most_frequent_words(words_frequency)
+    except (IndexError, FileNotFoundError):
+        print('Input file is not specified or missed')
+    except UnicodeDecodeError:
+        print('Incorrect file')
