cc4c9052c9e1d6d0a3b96aa0b536fd4e931c7d7a
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-01-05 00:05:51 +0300

    Переделал с использованием collections.Counter вместо словаря.

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,46 +1,24 @@
 import sys
-import string
-
+import re
+import collections
 
 def load_data(filepath):
-    words_count = {}
     with open(filepath) as input_file:
-        for line in input_file:
-            for word in line.translate(
-                    ''.maketrans(
-                        '',
-                        '',
-                        string.punctuation
-                    )
-            ).split():
-                words_count[word.lower()] = words_count.get(word, 0) + 1
-    return words_count
+        return input_file.read().lower()
 
 
 def get_most_frequent_words(file_content):
-    words_frequency = [
-        (frequency, word)
-        for word, frequency in file_content.items()
-    ]
-    words_frequency.sort()
-    words_frequency.reverse()
-    words_frequency = [
-        (word, frequency)
-        for frequency, word in words_frequency
-    ]
-    return words_frequency
-
-
-def print_most_frequent_words(words_frequency):
-    for word, frequency in words_frequency[:10]:
-        print(word, frequency)
+    words = re.findall(r'\w+', file_content)
+    most_frequent_words = collections.Counter(words).most_common(10)
+    return most_frequent_words
 
 
 if __name__ == '__main__':
     try:
         file_content = load_data(sys.argv[1])
-        words_frequency = get_most_frequent_words(file_content)
-        print_most_frequent_words(words_frequency)
+        most_frequent_words = get_most_frequent_words(file_content)
+        for word, frequency in most_frequent_words:
+            print(word, frequency)
     except (IndexError, FileNotFoundError):
         print('Input file is not specified or missed')
     except UnicodeDecodeError:
